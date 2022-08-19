d2ce423f6c47cc07ccca15a8f184c46d66c497f7
jm_b <jmbanchero@gmail.com>
2022-08-19 18:44:16 +0100

    add initial

---
+++ src/components/layouts/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+
+    static defaultProps = {
+        title: 'Github Finder',
+        icon: 'fab fab-github'
+    }
+
+  render() {
+    return (
+     <nav className='navbar bg-primary'>
+        <h1 >
+            <i className={this.props.icon}></i>
+            {this.props.title}
+        </h1>
+     </nav>
+    )
+  }
+}
+
+export default NavBar

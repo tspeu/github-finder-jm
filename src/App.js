655411093f161795689bf459e23707e930d2570b
jm_b <jmbanchero@gmail.com>
2022-08-20 20:08:02 +0100

    add env local

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Users from './components/users/Users';
 import axios from 'axios'
 
 // const URL = 'https://jsonplaceholder.typicode.com/todos/1';
-const URL = 'https://api.github.com/users';
+const URL = `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
 class App extends Component {
 	state = {
 		users: [],

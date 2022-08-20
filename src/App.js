e3fae4c009a9b3754429a4ae01f69d6d4518e5aa
jm_b <jmbanchero@gmail.com>
2022-08-20 19:12:15 +0100

    add axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import { render } from '@testing-library/react';
+import { Component } from 'react';
 import './App.css';
 import NavBar from './components/layouts/NavBar';
 import Users from './components/users/Users';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar></NavBar>
-			<div className='container'>
-				<Users></Users>
+// const URL = 'https://jsonplaceholder.typicode.com/todos/1';
+const URL = 'https://api.github.com/users';
+class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	}
+
+	async componentDidMount  () {
+		this.setState({ loading: true});
+
+		const res = await axios.get(URL);
+		this.setState({
+			users: res.data,
+			loading: false
+		})
+		console.log(res?.data);
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<NavBar></NavBar>
+				<div className='container'>
+					<Users></Users>
+				</div>
 			</div>
-    </div>
-  );
+		);
+	}
+	
 }
 
 export default App;

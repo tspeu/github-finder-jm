002ac4b11dace54bae77d2602a03edc08408b84d
jm_b <jmbanchero@gmail.com>
2022-08-30 19:59:47 +0100

    add link to profile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/layouts/NavBar';
 import Alert from './components/layouts/Alert';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import User from './components/users/User';
 import About from './components/pages/About';
 import axios from 'axios'
 
@@ -15,6 +16,7 @@ import axios from 'axios'
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null,
 	}
@@ -44,6 +46,21 @@ class App extends Component {
 		console.log(res?.data?.items);
 	}
 
+	// get user data
+	getUser = async (username) => {
+		this.setState({ loading: true});
+		console.log(' username', username);
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+		&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+		this.setState({
+			user: res.data,
+			loading: false
+		})
+		console.log(res?.data?.items);
+	}
+
 	// clear user form state
 	clearUsers = () => {
 		console.log('App clearUsers');
@@ -60,7 +77,7 @@ class App extends Component {
 	}
 
 	render() {
-		const {users, loading } = this.state;
+		const {users, loading, user } = this.state;
 
 		return (
 			<Router>
@@ -81,11 +98,15 @@ class App extends Component {
 									</Fragment>
 
 							} />
-								{/* render={props => ( */}
-						
-								{/* )}  */}
-								{/* <Route path='/about' component={About} />		 */}
+					
 							<Route path="/about" element={<About/>}/>			
+							<Route path="/user/:login" element={
+							<User {...this.props} 
+							getUser={this.getUser} 
+							user={user}
+							loading={loading}
+							/>
+							}/>			
 						</Routes>
 					</div>
 				</div>
--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTytes from "prop-types";
-import { Link , NavLink} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const NavBar = ({icon,title}) => {
 	return (

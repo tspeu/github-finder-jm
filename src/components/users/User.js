cbd5fe007fdd156e073deca46108adb7a170e1c2
jm_b <jmbanchero@gmail.com>
2023-05-07 13:53:26 +0100

    test

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+// import { useParams } from "react-router-dom";
 
 class User extends Component {
+	
+	componentDidMount() {
+		// let params = useParams();
+		// const { params } = this.props.params;
+		// this.props.getUser(this.props.match.params.login);
+		console.log('params')
+	}
+
+
 	render() {
+		const {
+			name,
+			avatar_url,
+			location,
+			bio,
+			blog,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user;
+
 		return (
 			<div>
-				uSER
+				USER
 			</div>
 		);
 	}
